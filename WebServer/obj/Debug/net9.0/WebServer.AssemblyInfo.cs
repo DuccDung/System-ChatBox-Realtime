18922022837ad441e007b1fdd70c8183cd60618a
DuccDung <duccdung999@gmail.com>
2026-02-24 15:37:18 +0700

    fix mobile thread list

--- WebServer/obj/Debug/net9.0/WebServer.AssemblyInfo.cs
+++ WebServer/obj/Debug/net9.0/WebServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb2e0ef5515ff9ac9efaac6fb3916f6c2ccbb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a204fd6c76d4d43b8cce18c17e7681606e2bbed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
